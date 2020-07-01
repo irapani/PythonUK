3d5374781485e1dc67e447e232c4dd4b7b43a36d
uirapani <Uday.crazyuk@gmail.com>
2020-06-30 22:14:46 -0700

    Dictionaries

--- Tuples.py
+++ Tuples.py
@@ -1,2 +1,14 @@
+#tuple cant be modified
 
+number = (1,2,3)
+print(number[0])
 
+#unpacking
+
+coordinates = (1,2,3)
+# x = coordinates[0]
+# y = coordinates[0]
+# z = coordinates[0]
+
+x,y,z = coordinates
+print(x,y,z)
