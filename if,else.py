03e9fa6ab32c5eaa9c708523780beb0ce8e273d3
irapani <uday.crazyuk@gmail.com>
2020-07-05 01:17:04 -0700

    Add files via upload

---
+++ if,else.py
@@ -0,0 +1,12 @@
+#If, if else
+
+a=20
+if a>50:
+    print("this is the if body")
+print("this is out side the if block")
+
+a = 60
+if a > 50:
+    print("this is the if body")
+print("this is out side the if block")
+
