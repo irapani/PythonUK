bf0e498647451d2dec9f8680bfb0f1fdbe5b9301
irapani <Uday.crazyuk@gmail.com>
2021-07-18 23:29:50 -0700

    updated

---
+++ Loops.py
@@ -0,0 +1,27 @@
+# For
+
+range(0, 10)
+
+for x in range(5, 16):
+    print(x)
+
+
+# While
+meter = 5
+while(meter<=10):
+    print("meter is ", meter)
+    meter+=1
+print("while loop finished..")
+
+# Add
+
+total = 0
+
+for count in range(3):
+    num = int (input("enter a number \n"))
+    total = total + num
+
+print("Total is ", total)
+
+# Enter the number in down you will get the out put
+
--- list.py
+++ list.py
@@ -7,11 +7,18 @@ lucky_numbers = [2,8,4,6,5,7]
 friends.extend(lucky_numbers)
 friends.append("jaya")
 friends[0] = "kranthi"
-friends.sort()
-lucky_numbers.reverse()
 
+for x in friends:
+    print(x)
 
 print (friends)
 print (friends.count("kumar"))
 
 
+fruits = ["Apple", "Mango", "Banana", "Cherry"]
+print(fruits)
+
+fruits[1] = "lychee"
+
+for item in fruits:
+    print(item)
